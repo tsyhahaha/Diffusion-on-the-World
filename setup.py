db17ea834b5a634107c727018f3e855157f90f49
tsyhahaha <tsy1433701769@163.com>
2024-03-12 11:35:07 +0800

    init package setting

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Diffusion-on-the-World',
+    version = '0.0.0',
+    license='MIT',
+    description = 'Use diffusion to solve general simplified problem on creating world.',
+    author = 'tsyhahaha',
+    author_email = 'tsy1433701769@163.com',
+    
+    packages=find_packages(),
+    # include_package_data=True,
+    # package_data={},
+    install_requires=[],
+)
